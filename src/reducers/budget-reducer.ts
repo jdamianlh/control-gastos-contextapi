2a40025c35661d772eceea47d76e33e39736c1c8
Damian <jdamianlh@gmail.com>
2024-07-23 22:07:45 -0500

    subiendo todo el proyecto

---
+++ src/reducers/budget-reducer.ts
@@ -0,0 +1,46 @@
+
+export type BudgetActions = 
+    {type: 'add-budget', payload: {budget: number}} |
+    {type: 'show-modal'} |
+    {type: 'close-modal'}
+
+export type BudgetState = {
+    budget: number
+    modal: boolean
+}
+
+export const initialState: BudgetState = {
+    budget: 0,
+    modal: false
+}
+
+export const budgetReducer = (
+    state: BudgetState = initialState,
+    action: BudgetActions
+) => {
+
+    switch(action.type){
+
+        case 'add-budget':{
+            return{
+                ...state,
+                budget: action.payload.budget
+            }
+        }
+        case 'show-modal':{
+            return{
+                ...state,
+                modal: true
+            }
+        }
+        case 'close-modal':{
+            return{
+                ...state,
+                modal: false
+            }
+        }
+        default: {
+            return state
+        }
+    }
+}
